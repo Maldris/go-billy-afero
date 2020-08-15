c894d1d0ea91f947913474699ff2bbea413a282d
Scott Owens <sdark.arrow@gmail.com>
2020-08-15 19:37:27 +1000

    test for ReadDir

--- afero_test.go
+++ afero_test.go
@@ -230,3 +230,61 @@ func TestOpenFile(t *testing.T) {
 		return
 	}
 }
+
+func TestReadDir(t *testing.T) {
+	sts, err := testFs.ReadDir("dir")
+	if err != nil {
+		t.Error("Error reading directory: ", err)
+		return
+	}
+
+	if len(sts) != 4 {
+		t.Error("Not the expected number of files found")
+		return
+	}
+
+	type statResult struct {
+		found bool
+		isDir bool
+	}
+
+	found := map[string]statResult{
+		"file1":  statResult{found: false, isDir: false},
+		"file.2": statResult{found: false, isDir: false},
+		"3file":  statResult{found: false, isDir: false},
+		"nested": statResult{found: false, isDir: true},
+	}
+
+	for _, st := range sts {
+		expect := found[st.Name()]
+		if st.IsDir() != expect.isDir {
+			t.Error(st.Name() + " does not match the expected file/directory status")
+		}
+		expect.found = true
+		found[st.Name()] = expect
+	}
+
+	for name := range found {
+		if !found[name].found {
+			t.Error("Expected file " + name + " not found")
+		}
+	}
+}
+
+func TestRename(t *testing.T) {
+	err := testFs.Rename("dir/nested/renameMe", "dir/nested/renamed")
+	if err != nil {
+		t.Error("Error renaming file: ", err)
+		return
+	}
+
+	data, err := afero.ReadFile(testFs.fs, "dir/nested/renamed")
+	if err != nil {
+		t.Error("Error reading renamed file", err)
+		return
+	}
+
+	if string(data) != dirFileCont1 {
+		t.Error("Renamed file content is not that of original file")
+	}
+}
